0b7bae2695d17439d4cbdaa29b4688a6d3619dd4
Igor Reutov <vextrell@gmail.com>
2018-01-26 16:57:32 +0300

    1

--- app/src/main/java/com/example/ruireutov/organiser/SpinnerAdapter.java
+++ app/src/main/java/com/example/ruireutov/organiser/SpinnerAdapter.java
@@ -5,17 +5,14 @@ import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
-import com.example.ruireutov.organiser.DatabaseWorkers.DatabaseDefines;
+import com.example.ruireutov.organiser.databaseWorkers.DatabaseDefines;
 
 import java.util.HashMap;
 
